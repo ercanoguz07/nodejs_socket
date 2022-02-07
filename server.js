952972e6feaac7c0b5b8a6ea7390cfd3255c9b51
ercanoguz07 <52664615+ercanoguz07@users.noreply.github.com>
2022-02-07 09:24:46 +0300

    nodejs_server

---
+++ server.js
@@ -0,0 +1,28 @@
+const  express=require("express");
+var http=require("http");
+const app=express();
+const port=process.env.PORT || 5000;
+var server=http.createServer(app);
+var io=require("socket.io")(server);
+
+app.use(express.json());
+var clients={};
+
+io.on("connection",(socket)=>{
+    console.log("connected");
+    console.log(socket.id,"has joined");
+    socket.on("/message",(msg)=>{
+        let targetId=msg.targetId;
+        console.log(msg);
+        if(clients[targetId])clients[targetId].emit("message",msg);
+    });
+    socket.on("/signin",(id)=>{
+        console.log(id+" ercan");
+        clients[id]=socket;
+        //console.log(clients);
+    });
+   
+});
+server.listen(port,"0.0.0.0",()=>{
+    console.log("server started");   
+});
